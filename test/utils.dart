04e98c5ae5b7e1fc93e0459ab4aa471a80585a8d
Alexander Thomas <athom@google.com>
2021-02-01 15:56:02 +0100

    Fix info

--- test/utils.dart
+++ test/utils.dart
@@ -18,7 +18,7 @@ class ExpectClient extends MockClient {
         super(fn);
 
   factory ExpectClient() {
-    var client;
+    ExpectClient client;
     client = ExpectClient._((request) => client._handleRequest(request));
     return client;
   }
